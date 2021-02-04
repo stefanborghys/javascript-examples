7015882bca0a8366e98366e7d65f8be917060777
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-04 23:47:34 +0100

    - webpack, asset management. Imported CSV and XML data files.

--- webpack-examples/src/index.js
+++ webpack-examples/src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Logo from '../assets/images/logo.png';
+import Messages from '../assets/data/messages.xml';
+import Animals from '../assets/data/animals.csv';
 
 function component(){
     const element = document.createElement('div');
@@ -13,6 +15,9 @@ function component(){
     logo.classList.add('logo');
 
     element.appendChild(logo);
+
+    console.log('Messages:', Messages);
+    console.log('Animals:',Animals);
 
     return element;
 }
