9b21d5c52854e4da57cc8e60b3f8cb3410d9efd4
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-05 00:22:05 +0100

    - webpack, asset management. Learned how to import YAML, JSON and TOML files. Importing a JSON5 file type didn't work as it kept complaining about the content.

--- webpack-examples/src/index.js
+++ webpack-examples/src/index.js
@@ -4,6 +4,10 @@ import Logo from '../assets/images/logo.png';
 import Messages from '../assets/data/messages.xml';
 import Animals from '../assets/data/animals.csv';
 
+import toml from '../assets/data/data.toml';
+import yaml from '../assets/data/data.yaml';
+import json from '../assets/data/data.json';
+
 function component(){
     const element = document.createElement('div');
 
@@ -16,8 +20,23 @@ function component(){
 
     element.appendChild(logo);
 
-    console.log('Messages:', Messages);
-    console.log('Animals:',Animals);
+    console.log('XML Messages:', Messages);
+    console.log('CSV Animals:',Animals);
+
+    console.group();
+    console.log('TOML title:', toml.title);
+    console.log('TOML owner:', toml.owner.name);
+    console.groupEnd();
+
+    console.group();
+    console.log('YAML title:', yaml.title);
+    console.log('YAML owner:', yaml.owner.name);
+    console.groupEnd();
+
+    console.group();
+    console.log('JSON title:', json.title);
+    console.log('JSON owner:', json.owner.name);
+    console.groupEnd();
 
     return element;
 }
--- webpack-examples/webpack.config.js
+++ webpack-examples/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
     entry: './src/index.js',
@@ -26,7 +29,26 @@ module.exports = {
         {
             test: /\.xml$/i,
             use: ['xml-loader']
-        }
-    ]
+        },
+        {
+            test: /\.toml$/i,
+            type: 'json',
+            parser: {
+                parse: toml.parse
+            }
+        },
+        {
+            test: /\.yaml$/i,
+            type: 'json',
+            parser: {
+                parse: yaml.parse
+            }
+        },
+        {
+            test: /\.json5$/i,
+            parser: {
+                parse: json5.parse
+            }
+        }]
     }
 };
