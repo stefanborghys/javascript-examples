5e0024099a6ddae56cee79b4d49360147f2e6d35
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-04 22:59:49 +0100

    - webpack, asset management. Added background and logo as assets.

--- webpack-examples/src/index.js
+++ webpack-examples/src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Logo from '../assets/logo.png';
 
 function component(){
     const element = document.createElement('div');
 
     element.innerHTML= _.join(['Hello','world!'],' ');
     element.classList.add('hello');
+
+    const logo = new Image();
+    logo.src = Logo;
+    logo.classList.add('logo');
+
+    element.appendChild(logo);
 
     return element;
 }
