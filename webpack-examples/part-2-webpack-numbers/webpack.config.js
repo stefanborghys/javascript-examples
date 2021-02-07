fa7945f22f84e6828b86b616e54b56c76ebe4673
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-08 00:23:43 +0100

    - webpack, authoring libraries. Expose the library

--- webpack-examples/part-2-webpack-numbers/webpack.config.js
+++ webpack-examples/part-2-webpack-numbers/webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
 
-module.exports = ['source-map'].map((devtool) => ({
+module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'webpack-numbers.js'
+        filename: 'webpack-numbers.js',
+        library: 'webpackNumbers',
+        libraryTarget: 'umd'
     },
-    devtool,
-    optimization: {
-        runtimeChunk: true
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
     }
-}));
+};
