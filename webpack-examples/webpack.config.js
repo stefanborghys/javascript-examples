045bdbdafe7852dd97e3a60ac9a87b9461f315b0
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-07 19:03:22 +0100

    - webpack, development. Using source maps

--- webpack-examples/webpack.config.js
+++ webpack-examples/webpack.config.js
@@ -7,14 +7,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Webpack Example - Output Mangement',
+            title: 'Webpack Example - Development',
         }),
     ],
     output: {
