27eb5ece6c610b3ed9c07762ed822d66ec7581e5
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-07 19:22:33 +0100

    - webpack, development. Using webpack-dev-server

--- webpack-examples/webpack.config.js
+++ webpack-examples/webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
         print: './src/print.js'
     },
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     plugins: [
         new CleanWebpackPlugin({
             cleanStaleWebpackAssets: false
