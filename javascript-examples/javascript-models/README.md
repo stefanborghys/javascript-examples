1cc733899942ff047934d00dc6e0ba0f4336415f
stefanborghys <stefan.borghys@seriousbusiness.be>
2021-02-26 16:42:04 +0100

    - javascript models, added JSDoc configuration allowing the creation of documentation

--- javascript-examples/javascript-models/README.md
+++ javascript-examples/javascript-models/README.md
@@ -14,13 +14,20 @@
       3. add `dist` clean-up  
       `npm install --save-dev clean-webpack-plugin`
 3. Jest (test) setup
-      1. install Jest  
-      `npm install --save-dev jest`
-      2. install `babel-jest`  
-      `npm install --save-dev babel-jest`
-      3. add Jest configuration to `package.json` and add `babel-jest` as the transformer
-      4. install Babel JS  
-      Babel is needed to allow Jest to handle the EcmaScript modules used in the example.  
-      `npm install --save-dev @babel/core @babel/preset-env`
-      5. create and configure `babel.config.json` in the project's root folder
+   1. install Jest  
+   `npm install --save-dev jest`
+   2. install `babel-jest`  
+   `npm install --save-dev babel-jest`
+   3. add Jest configuration to `package.json` and add `babel-jest` as the transformer
+   4. install Babel JS  
+   Babel is needed to allow Jest to handle the EcmaScript modules used in the example.  
+   `npm install --save-dev @babel/core @babel/preset-env`
+   5. create and configure `babel.config.json` in the project's root folder
+4. JSDoc setup
+   1. install JSDoc  
+   `npm install --save-dev jsdoc`
+   2. add `document` script to the `package.json` 
+   This starts JSDoc from it's local `node_modules` location and points where to find it's cumstom config.
+   3. create and configure `jsdoc.conf.json`  
+   Contains custom JSDoc configuration.
 
--- javascript-examples/javascript-models/src/model/lambert-coordinaat.js
+++ javascript-examples/javascript-models/src/model/lambert-coordinaat.js
@@ -1,22 +1,94 @@
+/**
+ * @author stefan borghys
+ * @since 1.0.0
+ * 
+ * @public
+ * @class
+ * @classdesc Represents the Belgian Lambert Coördinaat.
+ * @hideconstructor
+ */
 class LambertCoordinaat {
 
+    /**
+     * Create a new LambertCoordinaat based on it's x and y values.
+     * 
+     * @param {*} x - x-coordinate
+     * @param {*} y - y-coordinate
+     * 
+     * @private
+     * @since 1.0.0
+     */
     constructor(x, y) {
         this._x = x;
         this._y = y;
     }
 
+    /**
+     * Get the x-coordinate.
+     * 
+     * @public
+     * @since 1.0.0
+     * @function
+     * @return {Number} Returns an x-coordinate
+     */
     get x() {
         return this._x;
     }
 
+    /**
+     * Get the y-coordinate.
+     * 
+     * @public
+     * @since 1.0.0
+     * @function
+     * @return {Number} Returns an y-coordinate
+     */
     get y() {
         return this._y;
     }
     
+    /**
+     * Represents the coordinate as a String value.
+     * 
+     * @example '104719.27, 192387.25'
+     * 
+     * @public
+     * @since 1.0.0
+     * @function
+     * @return {string} Returns a String value of the coordinate in following format 'x, y'     
+     */
     toString() {
         return this._x + ', ' + this._y;
     }
 
+     /**
+     * Creates a new LambertCoordinaat based upon a regular expression. 
+     * When the input value is invalid or not of type String, 'undefined' will be returned!
+     *
+     * The regular expression allows following:
+     * - optioneel ongelimiteerd aantal spaties voor de start van het coördinaat
+     * - optioneel linker haakje '('
+     * - verplichte x-coördinaat, welke een decimaal of geheel getal kan zijn met 1-6 getallen voor de komma en een ongelimiteerd aantal na
+     * - verplicht scheidingsteken, welke een komma ',' of puntkomma ';' kan zijn
+     * - optionele ongelimiteerd aantal spaties
+     * - verplichte y-coördinaat, welke een decimaal of geheel getal kan zijn met 1-6 getallen voor de komma en een ongelimiteerd aantal na
+     * - optioneel rechter haakje ')'
+     *
+     * @example LambertCoordinaat.of('104719.27, 192387.25')
+     * @example LambertCoordinaat.of('104719.27,192387.25')
+     * @example LambertCoordinaat.of('104719.27; 192387.25')
+     * @example LambertCoordinaat.of('104719.27, 192387.25)')
+     * @example LambertCoordinaat.of('(104719.27, 192387.25')
+     * @example LambertCoordinaat.of('104719, 192387')
+     *
+     * @param {string} value - String value to create a new LambertCoordinaat of
+     * @return {LambertCoordinaat|undefined} Returns a new LambertCoordinaat when valid, otherwise undefined
+     * 
+     * @since 1.0.0
+     * @static
+     * @instance
+     * @function of
+     */
     static of(value) {
         if (!value || (!value instanceof String)) {
             return undefined;
